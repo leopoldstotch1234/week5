8f478e064dc9ab04563414f55cd98d4ecf5e18f8
leopoldstotch1234 <157330070+leopoldstotch1234@users.noreply.github.com>
2024-03-21 21:46:56 -0400

    Update Calculator.java

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,12 +3,12 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 /**
- * Adding comment for week 5.
+ * added
  */
 
 @Service
 public class Calculator {
-        final static int UMLNUMBER1 = 3;
+        final static int UML_NUMBER_1 = 3;
 	@Cacheable("sum")
 	public int sum(int a, int b) {
 		return a + b;
